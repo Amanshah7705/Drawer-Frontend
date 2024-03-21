7b8a419e9a642f703e71e5557fc540877b8ad2a6
Amanshah7705 <20bce259@nirmauni.ac.in>
2024-03-21 14:37:53 +0530

    1

--- src/app/socket.tsx
+++ src/app/socket.tsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://drawer-frontend.vercel.app/", {
+export const socket = io("https://drawapp-backend.onrender.com", {
   withCredentials: true,
   extraHeaders: {
     "my-custom-header": "abcd",
