f16b1fb3f0f0b84065b2f4efb780ce9267e2770f
Amanshah7705 <20bce259@nirmauni.ac.in>
2024-03-21 15:03:33 +0530

    1

--- src/app/socket.tsx
+++ src/app/socket.tsx
@@ -2,7 +2,4 @@ import { io } from "socket.io-client";
 
 export const socket = io("https://drawapp-backend.onrender.com", {
   withCredentials: true,
-  extraHeaders: {
-    "my-custom-header": "abcd",
-  },
 });
